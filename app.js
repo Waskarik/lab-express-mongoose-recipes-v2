a90be5fe55c364dcc35341b7d8076a4c6dda456f
Leanderson Costa <exmoser@gmail.com>
2026-09-20 15:05:16 +0100

    done

--- app.js
+++ app.js
@@ -1,51 +1,74 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
 
 const app = express();
 
-// MIDDLEWARE
 app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
-// Iteration 1 - Connect to MongoDB
-// DATABASE CONNECTION
+mongoose
+  .connect(MONGODB_URI)
+  .then((connection) =>
+    console.log(`Connected to Mongo! Database name: "${connection.connections[0].name}"`)
+  )
+  .catch((error) => console.error("Error connecting to mongo", error));
 
-
-
-// ROUTES
-//  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
 
+app.post("/recipes", async (req, res) => {
+  try {
+    const recipe = await Recipe.create(req.body);
+    res.status(201).json(recipe);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-//  Iteration 3 - Create a Recipe route
-//  POST  /recipes route
+app.get("/recipes", async (req, res) => {
+  try {
+    const recipes = await Recipe.find();
+    res.status(200).json(recipes);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
+app.get("/recipes/:id", async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    res.status(200).json(recipe);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-//  Iteration 4 - Get All Recipes
-//  GET  /recipes route
+app.put("/recipes/:id", async (req, res) => {
+  try {
+    const recipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(recipe);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
+app.delete("/recipes/:id", async (req, res) => {
+  try {
+    await Recipe.findByIdAndDelete(req.params.id);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-//  Iteration 5 - Get a Single Recipe
-//  GET  /recipes/:id route
+app.listen(3000, () => console.log("My first app listening on port 3000!"));
 
-
-//  Iteration 6 - Update a Single Recipe
-//  PUT  /recipes/:id route
-
-
-//  Iteration 7 - Delete a Single Recipe
-//  DELETE  /recipes/:id route
-
-
-
-// Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
-
-
-
-//❗️DO NOT REMOVE THE BELOW CODE
 module.exports = app;
